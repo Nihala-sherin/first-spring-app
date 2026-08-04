26f04aa855e671c8634d85215a245e9b0fb3f2f8
*nihala* <nihalasherin254@gmail.com>
2026-08-04 18:33:00 +0530

    api getmapping done

--- src/main/java/com/firstspringboot/example/first_spring_app/controller/DemoController.java
+++ src/main/java/com/firstspringboot/example/first_spring_app/controller/DemoController.java
@@ -1,7 +1,20 @@
 package com.firstspringboot.example.first_spring_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//@ResponseBody
+@RestController
+@RequestMapping("/demo/apis")
 public class DemoController {
+
+    @GetMapping("/demo1") //endpoint
+    public String getDemo1(){
+        System.out.println("first demo api");
+        return "first demo api";
+    }
 }
--- src/main/java/com/firstspringboot/example/first_spring_app/controller/SampleController.java
+++ src/main/java/com/firstspringboot/example/first_spring_app/controller/SampleController.java
@@ -1,7 +1,25 @@
 package com.firstspringboot.example.first_spring_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//@ResponseBody
+@RestController
+@RequestMapping("/sample/apis")
 public class SampleController {
+
+    @GetMapping("/sample1") //endpoint
+    public String getSample1(){
+        System.out.println("first... sample api");
+        return "first... sample api";
+    }
+    @GetMapping("/sample1")
+    public String getSample2(){
+        System.out.println("second sample api");
+        return "second sample api";
+    }
 }
